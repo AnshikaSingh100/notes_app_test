04b4e77bf7f84658e5017fe1b8a4f5546774d2d3
Anshika <anshiikasiingh@gmail.com>
2024-04-28 19:10:55 +0530

    Added shared notes to get all notes api

--- api/Note_cruds.py
+++ api/Note_cruds.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializer import Userserialiser,Noteserialiser
-from modelapp.models import Users,Notes
+from .serializer import Userserialiser, Noteserialiser
+from modelapp.models import Users, Notes
+
+id_list = []
+
 
 @api_view(['GET'])
 def getallnotes(request):
@@ -9,37 +12,45 @@ def getallnotes(request):
         r = request.query_params['user']
     except KeyError as e:
         return Response('No user specified')
-    read={x for x in Notes.objects.values_list('Readaccess')}
+    # read={x for x in Notes.objects.values_list('Readaccess')}
 
+    regex = Notes.objects.all()
+    for i in regex:
+        if r in i.Readaccess.split(","):
+            id_list.append(i.id)
+    # id = {x for x in id_list}
+    # id1=",".join(map(str,id_list))
+    data1 = Notes.objects.filter(id__in=id_list)
     data = Notes.objects.filter(User=r)
-    serializer = Noteserialiser(data, many=True)
+    combined_qs = data | data1
+    serializer = Noteserialiser(combined_qs, many=True)
+
     return Response(serializer.data)
 
 
 @api_view(['GET'])
-def getanote(request,pk):
+def getanote(request, pk):
     r = request.query_params['user']
     # dataa = Notes.objects.filter(User=r)
 
     data = Notes.objects.filter(id=pk, User=r)
-    if len(data)==0:
+    if len(data) == 0:
         data_by_only_id = Notes.objects.filter(id=pk)
-        if len(data_by_only_id)==0:
+        if len(data_by_only_id) == 0:
             return Response("Not a valid note id")
-        myread={x for x in data_by_only_id[0].Readaccess if x == str(r)}
-        if len(myread)!=0:
+        myread = {x for x in data_by_only_id[0].Readaccess if x == str(r)}
+        if len(myread) != 0:
             serializer = Noteserialiser(data_by_only_id, many=True)
             return Response(serializer.data)
-
 
     serializer = Noteserialiser(data, many=True)
     return Response(serializer.data)
 
+
 @api_view(['POST'])
 def createnote(request):
-
     serializer = Noteserialiser(data=request.data)
-    print(serializer.is_valid(),serializer.errors)
+    print(serializer.is_valid(), serializer.errors)
     if request.data['User'] in [None, ""]:
         return Response("User Cannot be null or empty")
     print("We are going to save it")
@@ -47,9 +58,9 @@ def createnote(request):
     read_access_list = read_access_csv.split(',')
     for i in read_access_list:
         try:
-            check=Users.objects.get(id=i)
+            check = Users.objects.get(id=i)
         except Users.DoesNotExist:
-            return Response("User "+i+" does not exist")
+            return Response("User " + i + " does not exist")
 
     if serializer.is_valid():
         print("yes it is possible to save it")
@@ -59,23 +70,22 @@ def createnote(request):
 
 
 @api_view(['PUT'])
-def updatenote(request,pk):
+def updatenote(request, pk):
     r = request.query_params['user']
 
-
     try:
-        instance=Notes.objects.get(id=pk, User=r)
+        instance = Notes.objects.get(id=pk, User=r)
     except Notes.DoesNotExist:
-        return Response("There is no note created by you with noteid ="+pk)
-    serializer=Noteserialiser(instance,data=request.data)
-    print(serializer.is_valid(),serializer.errors)
+        return Response("There is no note created by you with noteid =" + pk)
+    serializer = Noteserialiser(instance, data=request.data)
+    print(serializer.is_valid(), serializer.errors)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
 
+
 @api_view(['DELETE'])
 def deleteanote(request, pk):
-
-    database=Notes.objects.get(id=pk)
+    database = Notes.objects.get(id=pk)
     database.delete()
     return Response()
